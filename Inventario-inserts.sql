1db934b603b4a70e3da4b6433fde8aa957c08f96
joelrrr2002 <81340144+joelrrr2002@users.noreply.github.com>
2022-08-08 00:29:19 -0600

    Add files via upload

---
+++ Inventario-inserts.sql
@@ -0,0 +1,13 @@
+use dt;
+
+insert into Inventario(id_inventario,id_biblioteca,num_libros,descripcion) values (1,1,500,NULL);
+insert into Inventario(id_inventario,id_biblioteca,num_libros,descripcion) values (2,2,600,NULL);
+insert into Inventario(id_inventario,id_biblioteca,num_libros,descripcion) values (3,1,400,NULL);
+insert into Inventario(id_inventario,id_biblioteca,num_libros,descripcion) values (4,2,350,NULL);
+insert into Inventario(id_inventario,id_biblioteca,num_libros,descripcion) values (5,1,550,NULL);
+insert into Inventario(id_inventario,id_biblioteca,num_libros,descripcion) values (6,2,450,NULL);
+insert into Inventario(id_inventario,id_biblioteca,num_libros,descripcion) values (7,1,650,NULL);
+insert into Inventario(id_inventario,id_biblioteca,num_libros,descripcion) values (8,2,650,NULL);
+insert into Inventario(id_inventario,id_biblioteca,num_libros,descripcion) values (9,1,750,NULL);
+insert into Inventario(id_inventario,id_biblioteca,num_libros,descripcion) values (10,2,750,NULL);
+insert into Inventario(id_inventario,id_biblioteca,num_libros,descripcion) values (11,1,850,NULL);
